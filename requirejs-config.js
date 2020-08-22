fceae46ec0f2b581148a7ac080c07e551722f6ac
DUC-ANH-GHEL <aduc7246@gmail.com>
2020-08-22 12:33:21 +0700

    help product

--- requirejs-config.js
+++ requirejs-config.js
@@ -1,12 +1,26 @@
 var config = {
     paths: {
         slick: 'js/slick.min',
-        spinner: 'js/spinner'
+        spinner: 'js/spinner',
+        owlcarousel: 'js/owl.carousel'
     },
     shim: {
         slick: {
             deps: ['jquery']
+        },
+        owlcarousel: {
+            deps: ['jquery']
         }
-      }
-   };
-   
+    }
+};
+
+// var config = {
+//     paths: {
+//         'owlcarousel': "js/owl.carousel.min"
+//     },
+//     shim: {
+//         'owlcarousel': {
+//             deps: ['jquery']
+//         }
+//     }
+// };
